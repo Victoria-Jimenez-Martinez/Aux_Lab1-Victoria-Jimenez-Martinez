b9c6754d3fc61b02dc2187efd45721f8a1b88a3f
Jimenez-Martinez-Victoria <edenvictoriajimenes@gmail.com>
2024-03-09 19:47:36 -0600

    LaboratorioAux

---
+++ SecretFile.ts
@@ -0,0 +1,9 @@
+(()=>{
+    class SecretFile {
+        secretMessage(){
+            console.log("Laboratorio 1_Auxiliatura de Seminario de Sistemas");
+        }
+    }
+    const secretFile = new SecretFile ();
+    secretFile.secretMessage();
+})();
